b7ca6167bb06616a46ea2fa56a8dd26e08216fb7
Seth Shober <seth.shober@gmail.com>
2017-02-21 17:58:50 -0800

    add random functionality. minor UI improvements.

--- controllers/quotes.js
+++ controllers/quotes.js
@@ -1,34 +1,51 @@
-var Quote      = require('../models/quote')
-var QuoteToday = require('../models/quote-today')
-var router     = require('express').Router()
+'use strict'
+
+const Quote      = require('../models/quote')
+const QuoteToday = require('../models/quote-today')
+const router     = require('express').Router()
 
 // get all quotes
-router.get('/quotes.json', function(req, res, next) {
+router.get('/quotes.json', (req, res, next) => {
   Quote.find()
-    .exec(function(err, quotes) {
+    .exec((err, quotes) => {
       if (err) return next(err)
       res.json(quotes)
     })
 })
 
 // get old (used) quotes
-router.get('/usedQuotes.json', function(req, res, next) {
-  Quote.find({used: true})
-    .exec(function(err, quotes) {
+router.get('/usedQuotes.json', (req, res, next) => {
+  const query = {used: true}
+  Quote.find(query)
+    .exec((err, quotes) => {
       if (err) return next(err)
       res.json(quotes)
     })
 })
 
 // get today's quote
-router.get('/today.json', function(req, res, next) {
+router.get('/today.json', (req, res, next) => {
   QuoteToday.find()
-    .exec(function(err, today) {
+    .exec((err, today) => {
       if (err) return next(err)
       res.json(today)
     })
 })
 
+// get random quote
+router.get('/random.json', (req, res, next) => {
+  Quote.count()
+    .exec((err, count) => {
+      if (err) return next(err)
+      let r = Math.floor(Math.random() * count)
+      Quote.find().limit(1).skip(r)
+        .exec((err, quote) => {
+          if (err) return next(err)
+          res.json(quote)
+        }) 
+    })
+  
+})
 
 
 module.exports = router
